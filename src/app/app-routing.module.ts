46392436a427c1ddab5b84b2b18acd6c7191c56a
ayush.srivastava <ayush.srivastava@triconitsolutions.com>
2023-09-20 14:48:31 +0530

    project setup login/signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'login'
+  },
+  {
+    path:'login',
+    loadChildren:()=>import("./component/auth/login/login.module").then(m=>m.LoginModule),
+  },
+  {
+    path:'signup',
+    loadChildren:()=>import("./component/auth/signup/signup.module").then(m=>m.SignupModule)
+  },
+  {
+    path:'homepage',
+    loadChildren:()=>import("./component/homepage/homepage.module").then(m=>m.HomepageModule),
+    canActivate:[AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
